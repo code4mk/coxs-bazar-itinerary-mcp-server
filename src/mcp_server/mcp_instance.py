1a6f18df0a5f38b50e2f5e99b683afb6d27b120d
Mostafa Kamal <hiremostafa@gmail.com>
2025-11-10 15:11:54 +0600

    feature: add cox mcp logo

--- src/mcp_server/mcp_instance.py
+++ src/mcp_server/mcp_instance.py
@@ -2,12 +2,20 @@ import os
 from fastmcp import FastMCP
 from mcp_server.config.auth_provider import get_auth_provider
 from dotenv import load_dotenv
+from mcp.types import Icon
 
 load_dotenv()
 
 is_oauth_enabled = os.environ.get("IS_OAUTH_ENABLED") == "true"
 mcp_config_context = {
-    "name": "Cox's Bazar AI Itinerary MCP"
+    "name": "Cox's Bazar AI Itinerary MCP",
+    "icons": [
+        Icon(
+            src="https://raw.githubusercontent.com/code4mk/coxs-bazar-itinerary-mcp-server/refs/heads/main/cox-mcp.jpeg",
+            mimeType="image/jpeg",
+            sizes=["48x48"]
+        )
+    ]
 }
 
 
